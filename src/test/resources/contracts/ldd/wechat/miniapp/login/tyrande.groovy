d8a86074640d8c28edc39a6c11e8d1661cd58ef0
Yuexiang Gao <yxgao@thoughtworks.com>
2018-04-25 16:35:51 +0800

    move content type header into response

--- src/test/resources/contracts/ldd/wechat/miniapp/login/tyrande.groovy
+++ src/test/resources/contracts/ldd/wechat/miniapp/login/tyrande.groovy
@@ -6,14 +6,12 @@ Contract.make {
     request {
         method 'POST'
         url $(producer("/loginViaWeChatMiniApp?code=Tyrande"))
-        headers {
-            contentType('application/json;charset=UTF-8')
-        }
     }
     response {
         status 200
         headers {
             header 'Authorization': "Bearer Tyrande"
+            contentType('application/json')
         }
         body(
                 [
